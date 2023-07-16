125fd8cf5ac7deb901dfd23d410f68ce04a856d6
A Umesh <aumesh685@gmail.com>
2023-07-16 12:40:35 +0530

    adding multiply.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+package letslearngit;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Product of Two numbers is " + (a*b));
+    }
+
+}
